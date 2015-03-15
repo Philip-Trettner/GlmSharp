c12c1822ec72fdadd201b65a6d9bed80d0259359
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2015-03-15 21:26:37 +0100

    Removed obsolete swizzle project

--- GlmSharp/GlmSharpSwizzle/swizzle.cs
+++
@@ -1,9 +0,0 @@
-﻿using GlmSharp;
-
-namespace GlmSharpSwizzle
-{
-    public static class swizzle
-    {
-        public static vec3 xyz(this vec4 v) => new vec3 {x = v.x, y = v.y, z = v.z};
-    }
-}
